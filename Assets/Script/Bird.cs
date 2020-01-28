8e31080b13b1ee4c0ed71f822f58c1bc892ced7d
Emil Skogsund <skogix@gmail.com>
2020-01-28 18:08:15 +0100

    Comments
    Comments

--- Assets/Script/Bird.cs
+++ Assets/Script/Bird.cs
@@ -1,28 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
     private bool isDead = false;
+    // callar rigidbody från gameobject
     private Rigidbody2D rb;
 
+    // forcen i FLAPPEN
     public float upForce = 200f;
 
-    // Start is called before the first frame update
     void Start()
     {
+        // i startup hämta rigidbody från objektet
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // inge FLAPS om du är död
         if (isDead == false)
         {
+            // 0 LMB 1 RMB
             if (Input.GetMouseButtonDown(0))
             {
+                // sätt rbs velocity till 0 så det är samma FLAPP varje gång
                 rb.velocity = Vector2.zero;
+                // adda forcen uppåt
                 rb.AddForce(new Vector2(0, upForce));
             }
         }
