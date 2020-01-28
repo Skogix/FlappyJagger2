a13beb81541fd02b434a01e3c22056cd6adc87d4
Emil Skogsund <skogix@gmail.com>
2020-01-28 18:04:57 +0100

    Bird
    Sprites och basic FLAP!

---
+++ Assets/Script/Bird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private bool isDead = false;
+    private Rigidbody2D rb;
+
+    public float upForce = 200f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(0, upForce));
+            }
+        }
+    }
+}
